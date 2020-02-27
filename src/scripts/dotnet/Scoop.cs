27f6b034100414b6ff05b47a5972c2d019627668
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2020-02-28 00:43:45 +0100

    Retry failed scoop installs.

--- src/scripts/dotnet/Scoop.cs
+++ src/scripts/dotnet/Scoop.cs
@@ -32,19 +32,30 @@ class Scoop : TaskBase
         return Task.Run(async () =>
         {
             int returnCode = 0;
-            foreach (var task in _scoopQueue.Select(cmd => ExecCommand($"scoop install {cmd}", cmd)))
+            foreach (var scoop in _scoopQueue)
             {
-                (int code, string output) res = await task;
+                for (int i = 0; i < 3; i++)
+                {
+                    (int code, string output) res = await ExecCommand($"scoop install {scoop}", scoop);
 
-                returnCode = res.code;
+                    returnCode = res.code;
 
-                if (res.code != 0)
-                    Console.ForegroundColor = ConsoleColor.Red;
-                else
-                    Console.ForegroundColor = ConsoleColor.Green;
+                    if (res.code != 0)
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    else
+                        Console.ForegroundColor = ConsoleColor.Green;
 
-                Console.WriteLine($"Scoop task {++_status} of {Tasks} (\"{res.output}\") finished" + (res.code != 0 ? " with an error." : ".") + Environment.NewLine);
-                Console.ResetColor();
+                    Console.WriteLine($"Scoop task {(i == 0 ? ++_status : _status)} of {Tasks} (\"{res.output}\") finished" + (res.code != 0 ? " with an error." : ".") + Environment.NewLine);
+                    Console.ResetColor();
+
+                    if (res.code == 0)
+                        break;
+                    else
+                    {
+                        Console.WriteLine($"Retrying Scoop task \"{scoop}\"" + Environment.NewLine);
+                        await ExecCommand($"scoop uninstall {scoop}", scoop);
+                    }
+                }
             }
 
             return returnCode;
