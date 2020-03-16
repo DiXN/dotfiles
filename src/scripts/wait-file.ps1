c5d018d3f9bd46aa38c3eadbdb134df99abd291d
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2020-03-16 03:33:15 +0100

    Refactor wait scripts.

--- src/scripts/wait-file.ps1
+++ src/scripts/wait-file.ps1
@@ -3,10 +3,18 @@ param (
   [String] $app
 )
 
-Write-Output "Waiting for $($app) ..."
+Write-Output "Waiting for $app ..."
+
+$count = 0
 
 while (!(Get-Command $app -ErrorAction SilentlyContinue)) {
   Start-Sleep -Milliseconds 250
+  $count++
+
+  if ($count -eq 240) {
+    Write-Output "Still waiting for $app ..."
+    $count = 0
+  }
 }
 
 Start-Sleep -Milliseconds 1000
--- src/scripts/wait-folder.ps1
+++ src/scripts/wait-folder.ps1
@@ -3,10 +3,18 @@ param (
   [String] $path
 )
 
-Write-Output "Waiting for $($path) ..."
+Write-Output "Waiting for $path ..."
+
+$count = 0
 
 while (!(Test-Path $path)) {
   Start-Sleep -Milliseconds 250
+  $count++
+
+  if ($count -eq 240) {
+    Write-Output "Still waiting for $path ..."
+    $count = 0
+  }
 }
 
 Start-Sleep -Milliseconds 1000
