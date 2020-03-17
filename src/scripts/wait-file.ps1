fe7aeff51c7679d4fd24d765583b47ee16abb762
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2020-03-17 13:18:53 +0100

    Skip configuration of MikTeX on CI again.

--- src/scripts/wait-file.ps1
+++ src/scripts/wait-file.ps1
@@ -1,22 +1,12 @@
 param (
   [Parameter(Mandatory = $true)]
-  [String] $app,
-  [bool] $scoop = $false
+  [String] $app
 )
 
 Write-Output "Waiting for $app ..."
 
-$count = 0
-
-while ($scoop ? !(Get-ChildItem "~\scoop\apps\$app" -ErrorAction SilentlyContinue) `
-              : !(Get-Command $app -ErrorAction SilentlyContinue)) {
+while (!(Get-Command $app -ErrorAction SilentlyContinue)) {
   Start-Sleep -Milliseconds 250
-  $count++
-
-  if ($count -eq 240) {
-    Write-Output "Still waiting for $app ..."
-    $count = 0
-  }
 }
 
 Start-Sleep -Milliseconds 1000
--- src/scripts/wait-folder.ps1
+++ src/scripts/wait-folder.ps1
@@ -5,16 +5,8 @@ param (
 
 Write-Output "Waiting for $path ..."
 
-$count = 0
-
 while (!(Test-Path $path)) {
   Start-Sleep -Milliseconds 250
-  $count++
-
-  if ($count -eq 240) {
-    Write-Output "Still waiting for $path ..."
-    $count = 0
-  }
 }
 
 Start-Sleep -Milliseconds 1000
