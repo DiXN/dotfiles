00bc9d7edaa0ebe8d9c656dbcc6bafd91b640b67
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2021-03-10 00:02:54 +0100

    Fix quotes.

--- linux/scripts/link.sh
+++ linux/scripts/link.sh
@@ -7,10 +7,10 @@ mkdir -p "$scripts_dir"
 
 DOT_DIR="$DOT_DIR/dotfiles"
 
-cp -s "$DOT_DIR/linux/scripts/*" "$scripts_dir"
+cp -r -s "$DOT_DIR/linux/scripts"/* "$scripts_dir"
 ln -s "$DOT_DIR/linux/.zshenv" "/home/$user/.zshenv"
 
 mkdir -p "/home/$user/.config/"
-cp -s "$DOT_DIR/linux/.config/*" "/home/$user/.config/"
+cp -r -s "$DOT_DIR/linux/.config"/* "/home/$user/.config/"
 ln -s "$DOT_DIR/linux/.gitconfig" "/home/$user/.gitconfig"
 
