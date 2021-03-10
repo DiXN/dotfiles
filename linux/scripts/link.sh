99c4831b612749fe7163e507fa718338f0d1ecda
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2021-03-10 23:21:28 +0100

    Add rofi config.

--- linux/scripts/link.sh
+++ linux/scripts/link.sh
@@ -7,12 +7,23 @@ mkdir -p "$scripts_dir"
 
 DOT_DIR="$DOT_DIR/dotfiles"
 
+# scripts
 cp -r -s "$DOT_DIR/linux/scripts"/* "$scripts_dir"
+
+# zsh
 ln -s "$DOT_DIR/linux/.zshenv" "/home/$user/.zshenv"
 
+# configs
 mkdir -p "/home/$user/.config/"
 cp -r -s "$DOT_DIR/linux/.config"/* "/home/$user/.config/"
+
+# git
 ln -s "$DOT_DIR/linux/.gitconfig" "/home/$user/.gitconfig"
+
+# rofi
+ln -s "$DOT_DIR/linux/launcher.rasi" "/home/$user/launcher.rasi"
+ln -s "$DOT_DIR/linux/theme.rasi" "/home/$user/theme.rasi"
+ln -s "$DOT_DIR/linux/global.rasi" "/home/$user/global.rasi"
 
 ls -a "$scripts_dir"
 ls -a "/home/$user/.config"
