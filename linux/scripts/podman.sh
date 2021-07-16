c9f661fa390a75d411dc534079debd62778b6edd
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2021-07-16 04:01:20 +0200

    Fix permissions and shebang for "podman.sh".

--- linux/scripts/podman.sh
+++ linux/scripts/podman.sh
@@ -1,7 +1,7 @@
-#!/user/env/bin bash
+#!/usr/bin/env bash
 
 echo "$(whoami):100000:65536" | sudo tee /etc/subuid
 echo "$(whoami):100000:65536" | sudo tee /etc/subgid
 
-usermod --add-subuids 165536-231072 --add-subgids 165536-231072 "$(whoami)"
+sudo usermod --add-subuids 165536-231072 --add-subgids 165536-231072 "$(whoami)"
 
