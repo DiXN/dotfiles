cdc44f681213357f539564b6ca43c5ed118aa778
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2022-01-17 01:33:57 +0100

    Add chaotic to Pacman configuration.

--- src/scripts/init.sh
+++ src/scripts/init.sh
@@ -18,12 +18,30 @@ fi
 # exit 1
 # fi
 
+echo "[Adding chaotic to Pacman configuration ...]"
+sudo pacman-key --init
+sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key FBA220DFC880C036
+sudo pacman --noconfirm -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+
+CORES="$(lscpu | awk '/^CPU\(s\):/{ print $2 }')"
+
+cat <<EOF | sudo tee -a /etc/pacman.conf
+Color
+CheckSpace
+ILoveCandy
+ParallelDownloads=$CORES
+
+[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist
+EOF
+
 readonly DOTFILES_DIR="/home/$(whoami)/Documents/repos"
 mkdir -p "$DOTFILES_DIR"
 
 pushd "$DOTFILES_DIR" || exit 1
 
-sudo pacman -S --noconfirm git
+sudo pacman -Syy --noconfirm git
 sudo pacman -S --noconfirm chezmoi
 chezmoi init --branch chezmoi https://github.com/DiXN/dotfiles.git -S "$DOTFILES_DIR/dotfiles"
 
