988603dca2a07def227f8ccafedb3d455df96bbd
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2023-03-05 00:33:52 +0100

    Add the ability to rerun "essentials.sh".

--- linux/scripts/essentials.sh
+++ linux/scripts/essentials.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # cache sudo password
-if [ -n "$CI" ]; then
+if [ -z "$CI" ]; then
   {
     while :; do sudo -v; sleep 59; done
     SUDO_LOOP=$!
@@ -9,12 +9,6 @@ if [ -n "$CI" ]; then
 fi
 
 readonly DOTFILES_DIR="/home/$(whoami)/Documents/repos/dotfiles"
-
-echo "[Setup Podman ...]"
-echo "$(whoami):100000:65536" | sudo tee /etc/subuid
-echo "$(whoami):100000:65536" | sudo tee /etc/subgid
-
-sudo usermod --add-subuids 165536-231072 --add-subgids 165536-231072 "$(whoami)"
 
 echo "[Install NetworkManager ...]"
 yay -S --noconfirm networkmanager
@@ -40,28 +34,41 @@ show-clock = false
 default-user-image = #face-smile
 xft-hintstyle = hintfull
 position = 19%,center 50%,center
-clock-format = 
+clock-format =
 panel-position = bottom
 hide-user-image = true
 EOF
 
-echo "[Installing zsh, antibody and tmux ...]"
-yay -S --noconfirm zsh tmux antibody-bin
-antibody bundle < ~/.zsh_plugin.txt > ~/.zsh_plugins.sh
+if [ -z "$RERUN" ]; then
+  echo "[Setup Podman ...]"
+  echo "$(whoami):100000:65536" | sudo tee /etc/subuid
+  echo "$(whoami):100000:65536" | sudo tee /etc/subgid
 
-echo "[Change login shell]"
-sudo chsh -s /usr/bin/zsh "$(whoami)"
+  sudo usermod --add-subuids 165536-231072 --add-subgids 165536-231072 "$(whoami)"
 
-echo "[Installing awesome config ...]"
-readonly AWESOME_PATH="/home/$(whoami)/.config/awesome"
-git clone --recursive "https://github.com/DiXN/awesome-cfg.git" "$AWESOME_PATH"
+  echo "[Installing zsh, antibody and tmux ...]"
+  yay -S --noconfirm zsh tmux antibody-bin
+  antibody bundle < ~/.zsh_plugin.txt > ~/.zsh_plugins.sh
 
-yay -S --noconfirm lua-pam-git
-sudo ln -s /usr/lib/lua-pam/liblua_pam.so /usr/lib/lua/5.4
+  echo "[Change login shell]"
+  sudo chsh -s /usr/bin/zsh "$(whoami)"
+
+  echo "[Installing awesome config ...]"
+  readonly AWESOME_PATH="/home/$(whoami)/.config/awesome"
+  git clone --recursive "https://github.com/DiXN/awesome-cfg.git" "$AWESOME_PATH"
+
+  yay -S --noconfirm lua-pam-git
+  sudo ln -s /usr/lib/lua-pam/liblua_pam.so /usr/lib/lua/5.4
+
+else
+  git -c "$AWESOME_PATH" pull
+fi
+
+sudo ln -sf "$DOTFILES_DIR/linux/scripts/system.vsh" /usr/bin
 
 echo "[Applying dotfiles ...]"
 
-if ! [ -z "$CI" ]; then
+if [ -n "$CI" ]; then
   chezmoi apply -k --force -S "$DOTFILES_DIR/dotfiles"
 else
   chezmoi apply -k --force --exclude=encrypted -S "$DOTFILES_DIR/dotfiles"
@@ -73,4 +80,3 @@ sudo systemctl enable piavpn.service
 
 echo "[Cleaning cache ...]"
 sudo pacman -Scc --noconfirm
-
