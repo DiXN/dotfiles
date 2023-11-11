0e5b314cce91df709d8c5c1cac15d2c9c6183720
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2023-11-11 03:41:34 +0100

    Update "Hyprland" config.

--- private_dot_config/hypr/scripts/executable_decrypt_chezmoi.sh
+++ private_dot_config/hypr/scripts/executable_decrypt_chezmoi.sh
@@ -3,18 +3,23 @@
 set -euo pipefail
 shopt -s nocasematch
 
-readonly DOTFILES_ROOT="$HOME/Documents/repos/dotfiles"
-git -C "$DOTFILES_ROOT" pull
+if [[ -f "$HOME/.first_boot" ]]; then
+  echo "fist boot"
+  rm -f "$HOME/.first_boot"
+  exit 1
+  readonly DOTFILES_ROOT="$HOME/Documents/repos/dotfiles"
+  git -C "$DOTFILES_ROOT" pull
 
-yay -S zenity --noconfirm
+  yay -S zenity --noconfirm
 
-PASSWD="$(zenity --password)"
+  PASSWD="$(zenity --password)"
 
-PASSWORD=$PASSWD expect -f "$DOTFILES_ROOT/linux/scripts/expected"
-
-EXIT_CODE=$?
-
-if [ $EXIT_CODE -ne 0 ]; then
   PASSWORD=$PASSWD expect -f "$DOTFILES_ROOT/linux/scripts/expected"
+
+  EXIT_CODE=$?
+
+  if [ $EXIT_CODE -ne 0 ]; then
+    PASSWORD=$PASSWD expect -f "$DOTFILES_ROOT/linux/scripts/expected"
+  fi
 fi
 
