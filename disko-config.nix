e5690b56391fd67cbfd23ba8c6b79bc25a8ccf86
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2025-04-09 00:48:16 +0200

    Add "disko-config".

---
+++ disko-config.nix
@@ -0,0 +1,73 @@
+{ config, lib, ... }:
+{
+  disko.devices = {
+    disk = {
+      my-disk = {
+        device = "/dev/sda";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "500M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            primary = {
+              size = "100%";  # Use all remaining space for LVM
+              content = {
+                type = "lvm_pv";
+                vg = "mainpool";
+              };
+            };
+          };
+        };
+      };
+    };
+    lvm_vg = {
+      mainpool = {
+        type = "lvm_vg";
+        lvs = {
+          thinpool = {
+            size = "95%";  # Use 95% of VG for thin-pool
+            lvm_type = "thin-pool";
+          };
+          root = {
+            lvm_type = "thinlv";
+            pool = "thinpool";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+              mountOptions = [ "defaults" ];
+            };
+          };
+          home = {
+            lvm_type = "thinlv";
+            pool = "thinpool";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/home";
+            };
+          };
+          nix = {
+            lvm_type = "thinlv";
+            pool = "thinpool";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/nix";
+              mountOptions = [ "noatime" ];  # Reduce writes--we don't care about access times
+            };
+          };
+        };
+      };
+    };
+  };
+}
