351c2d7bb6e5ccd5a785134ba82bff651d9eff22
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2025-04-09 03:29:01 +0200

    Setup nix cache.

--- linux/nix/configuration.nix
+++ linux/nix/configuration.nix
@@ -36,6 +36,18 @@
       auto-optimise-store = true;
       experimental-features = [ "nix-command" "flakes" ];
       trusted-users = [ "root" "mk" ];
+
+      # Configure binary caches
+      substituters = [
+        "https://cache.nixos.org"
+        "https://hyprland.cachix.org"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="c=
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
     };
     gc = {
       automatic = true;
